0959f9ec0de534999f364dc175e3e87a76cf99ca
Chee <ck_white@outlook.com>
2024-02-09 00:01:49 -0500

    arrow

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (number) => {
+    return 2000 / 100;
+  };
+  
+  const square = (number) => {
+    return number * number;
+  };
+  
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+
+console.log(divide(100)); // Output: 20
+console.log(square(2, 4)); // Output: 16
+console.log(add(3, 4)); // Output: 15
